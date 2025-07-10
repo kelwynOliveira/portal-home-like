e0380f909611cd5974e579c96fca6d4dd7271cfc
Kelwyn Oliveira <ksoliveira.eng@gmail.com>
2025-07-10 13:49:07 -0300

    home page centralize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="min-h-screen max-w-7xl mx-auto">{children}</div>
       </body>
     </html>
   );
