75b70e9abe566c226657df14e081bce9f75b22c1
Kelwyn Oliveira <ksoliveira.eng@gmail.com>
2025-07-29 22:28:43 -0300

    chore: add admin button to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Header from "./_home/header";
 import ClientHome from "./_home/client-home";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div className="p-8 space-y-8">
+      <div className="flex justify-end">
+        <Link
+          href="/admin"
+          className="uppercase text-sm text-white gradient-bg px-6 py-3 shadow-lg transition-all duration-200 ease-in-out hover:scale-105 hover:cursor-pointer"
+        >
+          Admin
+        </Link>
+      </div>
       <Header />
       <ClientHome />
     </div>
